7e7dbd2090fc8098fbd312d44bf5063a323cb735
Nastoiker <damirdambinov@gmail.com>
2023-05-31 16:49:14 +0400

    some

---
+++ resources/components/Admin/AdminVideoContainer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex mx-auto  flex-wrap sm:gap-[30px]">
+        <div class="mx-auto sm:mx-0"  v-for="v in videos">
+            <VideoData  :video="v"/>
+        </div>
+        <h1 v-if="videos.length===0" class="text-4xl">
+            Ничего не найдено
+        </h1>
+    </div>
+
+</template>
+<script setup>
+import VideoData from "@/components/Video/VideoData.vue";
+const props = defineProps({
+    videos: Array,
+})
+</script>
