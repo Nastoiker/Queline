475eee29036081aa22f214a5cc8d4d9b5cc7eca6
Nastoiker <damirdambinov@gmail.com>
2023-06-02 17:17:08 +0400

    videoallow

--- resources/components/Containers/VideoContainer.vue
+++ resources/components/Containers/VideoContainer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex mx-auto  flex-wrap sm:gap-[30px]">
-        <div class="mx-auto sm:mx-0"  v-for="v in videos.filter(v => v.is_moderated===1)" :key="v.hash_id">
+        <div class="mx-auto sm:mx-0"  v-for="v in videos.filter(s => s.is_moderated===1)" :key="v.hash_id">
             <VideoData  :video="v"/>
         </div>
         <h1 v-if="videos.filter(v => v.is_moderated===1).length===0 && !show" class=" mx-auto text-4xl">
