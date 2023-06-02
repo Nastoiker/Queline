8b302cba82123cbb10ec97938100dbc3228c26d3
atimrish <atimrish11@gmail.com>
2023-06-02 20:09:55 +0400

    deletefix

--- resources/components/User/UserVideoContainer.vue
+++ resources/components/User/UserVideoContainer.vue
@@ -3,8 +3,8 @@
         <div class="mx-auto sm:mx-0"  v-for="v in videosarr.filter(video => (!deletedVideo.includes(video.hash_id) && video.ban_status_id!==4))">
             <VideoData  :video="v"/>
             <div>
-                <button @click="this.handleDelete(v.hash_id)" class=" text-white bg-red px-5 py-2 rounded-lg my-2 hover:scale-110 transition-all">Удалить видео</button>
-                <span class="mx-2">{{v.is_moderated===1 ?  'Прошло модерацию ✅ ' : 'На проверке &#128164;'}}</span>
+                <button @click="handleDelete(v.hash_id)" class=" text-white bg-red px-5 py-2 rounded-lg my-2 hover:scale-110 transition-all">Удалить видео</button>
+                <span class="mx-2">{{v.is_moderated===1 ?  'Промодерировано ✅ ' : 'На проверке &#128164;'}}</span>
             </div>
         </div>
         <h1 v-if="videosarr.length===0" class=" mx-auto text-4xl">
